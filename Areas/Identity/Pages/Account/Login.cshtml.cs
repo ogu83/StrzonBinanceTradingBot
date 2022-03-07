d216c72adb5a19a2a744b1750f80fd9f291e9b52
oguzkoroglu <oguzkoroglu@gmail.com>
2022-03-07 13:09:02 +0300

    Better regex email validator implemented

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,6 +66,7 @@ namespace StrzonBinanceTradingBot.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [EmailAddress]
+            [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
             public string Email { get; set; }
 
             /// <summary>
